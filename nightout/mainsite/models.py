929832e89a6a61fe129aa31471aea42bb42b653c
ee12082 <ee12082@fe.up.pt>
2018-04-14 22:04:25 +0100

    Events returning to feed

--- nightout/mainsite/models.py
+++ nightout/mainsite/models.py
@@ -3,33 +3,28 @@ from django.utils.timezone import now
 import datetime
 
 # Create your models here.
-# class Night(models.Model):
 
 class Night(models.Model):
-    id = models.CharField(max_length=30, primary_key=True)
     events = models.CharField(max_length=140, blank=False)
 
 class User(models.Model):
-    id = models.CharField(max_length=30, primary_key=True)
     name = models.CharField(max_length=30) 
     oauthkey = models.CharField(max_length=30)
 
     friends = models.ManyToManyField('self')
 
 class Events(models.Model): 
-    id = models.CharField(max_length=30, primary_key=True)
     description = models.CharField(max_length=140, blank=False)
     image = models.FileField(upload_to='upload/')
-    time = models.DateTimeField(max_length=30, default=now)
-    date = models.DateTimeField(max_length=30, default=now)
+    time = models.TimeField(max_length=30, default=now)
+    date = models.DateField(max_length=30, default=now)
     local = models.CharField(max_length=30)
     private = models.BooleanField(default=False)
     price = models.IntegerField(default=0)
 
-    users = models.ManyToManyField(User)
+    users = models.ManyToManyField(User, related_name="events")
 
 class Attending(models.Model):
-    id = models.CharField(max_length=30, primary_key=True)
     evento = models.ForeignKey(Events,
                                default='',
                                on_delete=models.CASCADE)
@@ -37,7 +32,6 @@ class Attending(models.Model):
                              default='',
                              on_delete=models.CASCADE)
 class Friends(models.Model):
-    id = models.CharField(max_length=30, primary_key=True)
     user1 = models.ForeignKey(User,
                               default='',
                               related_name='user1',
--- nightout/mainsite/views.py
+++ nightout/mainsite/views.py
@@ -6,7 +6,10 @@ from  .forms import EventForm, NightForm
 from .models import *
 
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
+import random
+import itertools
 
 def index(request):
     title = 'nightout'
@@ -16,8 +19,10 @@ def index(request):
     if not user.is_authenticated:
         return redirect('login')
 
-    context = {'title' : title, 'sitename' : sitename, 'user':user}
+    userEvents = feedEvents(user)
 
+    context = {'title' : title, 'sitename' : sitename, 'user':user, 'items':userEvents}
+    
     return render(
         request,
         'mainsite.html',
@@ -55,3 +60,33 @@ def myEvents(request):
 class UserDetailView(DetailView):
     def user_detail(request):
         redirect(index) 
+
+def feedEvents(user):
+    now = timezone.now()
+
+    userQ = User.objects.get(pk=user.id)
+    attending = userQ.events.filter(date__gte=now).order_by('date')[:10]
+    notAttending = Events.objects.exclude (users__id = user.id, date__lt=now).order_by('date')[:10]
+    feedEvents = list(itertools.chain(attending, notAttending))
+    eventsShuffled = sorted(feedEvents, key=lambda x: random.random())[:10]
+    
+    results={}
+    for event in eventsShuffled:
+        eventDict={}
+        if event in attending:
+            eventDict['status'] = 'Going'
+        else:
+            eventDict['status']= 'Not Going'
+        
+        goingTo = event.users.values_list('id',flat=True)
+        f = userQ.friends.filter(id__in = goingTo)        
+        
+        eventDict['friends'] = f
+        results[event] = eventDict
+
+    return results
+    #Only upcoming events (E se forem eventos a decorrer?)
+
+    
+
+
