47b8976454c52a7b02e0a3092d188e494f8395ad
rubensfig <r.figueiredo.52@gmail.com>
2018-04-15 08:09:32 +0100

    prod fix

--- nightout/nightout/production.py
+++ nightout/nightout/production.py
@@ -32,7 +32,6 @@ DATABASES = {
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -146,8 +145,7 @@ USE_I18N = True
 USE_L10N = True 
  
 USE_TZ = True 
- 
- 
+
 # Static files (CSS, JavaScript, Images) 
 # https://docs.djangoproject.com/en/2.0/howto/static-files/ 
  
