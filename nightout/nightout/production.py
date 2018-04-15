19c6808c147a72d5d581fe936ad1c681c2e1e38b
rubensfig <r.figueiredo.52@gmail.com>
2018-04-15 11:01:12 +0100

    odfna

--- nightout/nightout/production.py
+++ nightout/nightout/production.py
@@ -151,7 +151,7 @@ USE_TZ = True
  
 STATIC_ROOT = '/opt/nightout/static' 
 STATIC_URL = '/static/' 
-STATICFILES_DIRS = ( os.path.join('../static'), ) 
+STATICFILES_DIRS = ( os.path.join('static'), ) 
  
 LOGIN_URL = 'login' 
 LOGOUT_URL = 'index' 
