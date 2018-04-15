972e5ec5e8064d4670652063b914c1ea5a02b6eb
rubensfig <r.figueiredo.52@gmail.com>
2018-04-15 10:55:14 +0100

    prodes

--- nightout/nightout/production.py
+++ nightout/nightout/production.py
@@ -149,9 +149,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images) 
 # https://docs.djangoproject.com/en/2.0/howto/static-files/ 
  
-STATIC_ROOT = '' 
+STATIC_ROOT = '/opt/nightout/static' 
 STATIC_URL = '/static/' 
-STATICFILES_DIRS = ( os.path.join('static'), ) 
+STATICFILES_DIRS = ( os.path.join('../static'), ) 
  
 LOGIN_URL = 'login' 
 LOGOUT_URL = 'index' 
